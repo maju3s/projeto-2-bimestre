8bcc1e3e09f347fec7f69d2634c22614f929c4ef
ravenaRRR <raphaelamirandola@hotmail.com>
2022-05-29 19:59:02 -0300

    commit rapha 29.05 (19:57)

--- index.js
+++ index.js
@@ -19,6 +19,27 @@ $(document).ready(function() {
     });
 
 
+    //OWL
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        dots: true,
+        lazyLoad: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 2,
+            },
+        },
+    });
 
 
 });
